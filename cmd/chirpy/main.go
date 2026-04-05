3a3bfe0c6ee095c216cc58354b3eb5482b1ce617
Jeff Wilson <rjp.wilson@gmail.com>
2026-04-05 15:06:41 -0400

    Add migrate command

--- cmd/chirpy/main.go
+++ cmd/chirpy/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
@@ -10,9 +13,11 @@ import (
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
 	"github.com/rjpw/bootdev-chirpy/internal/api"
 	"github.com/rjpw/bootdev-chirpy/internal/config"
 	"github.com/rjpw/bootdev-chirpy/internal/metrics"
+	"github.com/rjpw/bootdev-chirpy/internal/schema"
 	"github.com/rjpw/bootdev-chirpy/internal/store/postgres"
 )
 
@@ -23,6 +28,12 @@ type AppEnvironment struct {
 }
 
 func main() {
+
+	if len(os.Args) > 1 && os.Args[1] == "migrate" {
+		runMigrate(os.Args[2:])
+		return
+	}
+
 	env, err := createEnvironment()
 	if err != nil {
 		log.Fatalf("Failed to create config: %v", err)
@@ -65,6 +76,65 @@ func createEnvironment() (*AppEnvironment, error) {
 		DBURL:    os.Getenv("DB_URL"),
 		Platform: os.Getenv("PLATFORM"),
 	}, nil
+}
+
+func runMigrate(args []string) {
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "usage: chirpy migrate [up|status]\n")
+		os.Exit(1)
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		fmt.Fprintf(os.Stderr, "DB_URL is required\n")
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "open db: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	migrationsFS, err := fs.Sub(schema.Migrations, "migrations")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "migrations fs: %v\n", err)
+		os.Exit(1)
+	}
+
+	provider, err := goose.NewProvider(goose.DialectPostgres, db, migrationsFS)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "goose provider: %v\n", err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	switch args[0] {
+	case "up":
+		results, err := provider.Up(ctx)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "migrate up: %v\n", err)
+			os.Exit(1)
+		}
+		for _, r := range results {
+			fmt.Printf("applied: %s (%s)\n", r.Source.Path, r.Duration)
+		}
+	case "status":
+		results, err := provider.Status(ctx)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "migrate status: %v\n", err)
+			os.Exit(1)
+		}
+		for _, r := range results {
+			fmt.Printf("%-5s %s\n", r.State, r.Source.Path)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "unknown migrate command: %s\n", args[0])
+		fmt.Fprintf(os.Stderr, "usage: chirpy migrate [up|status]\n")
+		os.Exit(1)
+	}
 }
 
 func runUntilInterrupt(srv *http.Server) error {
